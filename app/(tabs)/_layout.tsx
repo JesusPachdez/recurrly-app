47a8119b743ffcd22a87cacb45979ba99ba8f0bb
Jesús Pacheco Valdez <pacheco1290@gmail.com>
2026-05-04 18:49:36 -0600

    feat(tabs): configurable tab bar with icon map, clsx, and asset typings

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,34 @@
+import { tabs } from "@/constants/data";
+import clsxClasses from "clsx";
 import { Tabs } from "expo-router";
+import { Image, View } from "react-native";
 
 const TabsLayout = () => {
+  const RenderTabIcon = ({ focused, icon }: TabIconProps) => {
+    return (
+      <View className={"tabs-icon"}>
+        <View className={clsxClasses("tabs-pill", focused && "tabs-active")}>
+          <Image source={icon} className={"tabs-glyph"} />
+        </View>
+      </View>
+    );
+  };
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="subscriptions" options={{ title: "Subscriptions" }} />
-      <Tabs.Screen name="insights" options={{ title: "Insights" }} />
-      <Tabs.Screen name="settings" options={{ title: "Settings" }} />
+    <Tabs screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+      {tabs.map((tab) => {
+        return (
+          <Tabs.Screen
+            key={tab.name}
+            name={tab.name}
+            options={{
+              title: tab.title,
+              tabBarIcon: ({ focused }) => (
+                <RenderTabIcon focused={focused} icon={tab.icon} />
+              ),
+            }}
+          />
+        );
+      })}
     </Tabs>
   );
 };
